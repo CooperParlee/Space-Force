aece7e5a5dc3164687cff1803f4702bcf77eb882
CodeChicken <33497381+ChickenCore@users.noreply.github.com>
2019-04-19 19:51:24 -0400

    Laser targets mouse rather than straight ahead

--- Assets/Laser.cs
+++ Assets/Laser.cs
@@ -8,15 +8,17 @@ public class Laser : MonoBehaviour {
     float range; 
 
     Vector3 origin;
+    Vector3 target;
     private const float xOffset = -0.6f;
     private const float yOffset = -2f;
 
-    public void Init(float angle, float speed = 40f, float range = 100f)
+    public void Init(float angle, Vector3 target, float speed = 40f, float range = 100f)
     {
-        this.angle = -angle;
+        this.origin = gameObject.transform.position + new Vector3(yOffset * Mathf.Sin(angle * Mathf.Deg2Rad) + xOffset * Mathf.Cos(angle * Mathf.Deg2Rad), -yOffset * Mathf.Cos(angle * Mathf.Deg2Rad) + xOffset * Mathf.Sin(angle * Mathf.Deg2Rad)); //Right turret new Vector3(yOffset * Mathf.Sin(angle * Mathf.Deg2Rad), -yOffset * Mathf.Cos(angle * Mathf.Deg2Rad));
+        this.angle = Mathf.Atan((target.y - origin.y) / (target.x - origin.x)) * Mathf.Rad2Deg - angle;
         this.speed = speed;
         this.range = range;
-        this.origin = gameObject.transform.position + new Vector3(yOffset * Mathf.Sin(angle * Mathf.Deg2Rad) + xOffset * Mathf.Cos(angle * Mathf.Deg2Rad), -yOffset * Mathf.Cos(angle * Mathf.Deg2Rad) + xOffset * Mathf.Sin(angle * Mathf.Deg2Rad)); //Right turret new Vector3(yOffset * Mathf.Sin(angle * Mathf.Deg2Rad), -yOffset * Mathf.Cos(angle * Mathf.Deg2Rad));
+        this.target = target;
         gameObject.transform.eulerAngles = new Vector3(0f, 0f, angle + 90f);
         gameObject.transform.position = origin;
         gameObject.name = "Laser Beam";
