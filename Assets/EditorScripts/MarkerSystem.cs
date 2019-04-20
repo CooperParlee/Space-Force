25675f9b85d8b9040b4e7c073549574714313445
CodeChicken <33497381+ChickenCore@users.noreply.github.com>
2019-04-20 19:55:30 -0400

    Started marker system

---
+++ Assets/EditorScripts/MarkerSystem.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections.Generic;
+
+public class MarkerSystem : EditorWindow {
+    private const float version = 0.1f;
+
+    List<MarkerItem> guiItems = new List<MarkerItem>();
+
+    [MenuItem("Window/MarkerSystem")]
+    public static void ShowWindow()
+    {
+        GetWindow<MarkerSystem>("MarkerSystem " + version);
+    }
+
+    private void OnGUI()
+    {
+        if (GUILayout.Button("+"))
+        {
+            guiItems.Add(new MarkerItem("Marker1", new Color()));
+        }
+        EditorGUILayout.Foldout(true, "Active Markers:");
+        EditorGUILayout.BeginScrollView(new Vector2());
+        foreach(MarkerItem item in guiItems)
+        {
+            EditorGUILayout.BeginHorizontal();
+            EditorGUILayout.LabelField(item.GetName(), GUILayout.Width(100f), GUILayout.ExpandWidth(false));
+            if (GUILayout.Button("-"))
+            {
+                guiItems.Remove(item);
+                break;
+            }
+            EditorGUILayout.EndHorizontal();
+        }
+        EditorGUILayout.EndScrollView();
+    }
+}
+
+public class MarkerItem
+{
+    string name;
+    Color color;
+
+    public MarkerItem(string name, Color color)
+    {
+        this.name = name;
+        this.color = color;
+    }
+
+    public string GetName() { return name; }
+    public Color GetColor() { return color; }
+}
--- Assets/Laser.cs
+++ Assets/Laser.cs
@@ -3,41 +3,40 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Laser : MonoBehaviour {
-    float rawAngleToTarget;
     float speed;
     float range;
-    float newAngle;
+    float angle;
 
     Vector3 origin;
     Vector3 target;
     private const int rangeOfMotion = 15;
 
-    public void Init(float shipAngle, Vector3 target, float speed = 40f, float range = 100f)
+    public void Fire(float shipAngle, Vector3 origin, Vector3 target, float speed = 40f, float range = 100f)
     {
         this.speed = speed;
         this.range = range;
         this.target = target;
         shipAngle = shipAngle + 90f;
 
-        this.origin = gameObject.transform.position;
-        this.rawAngleToTarget = Mathf.Atan2(target.y - origin.y, target.x - origin.x) * Mathf.Rad2Deg;
+        this.origin = origin;
+        float rawAngleToTarget = Mathf.Atan2(target.y - origin.y, target.x - origin.x) * Mathf.Rad2Deg;
         float error = rawAngleToTarget - shipAngle;
         //this.rawAngleToTarget = Mathf.Max(Mathf.Min(this.rawAngleToTarget, rangeOfMotion), -rangeOfMotion);
-        this.newAngle = shipAngle + error;
+        this.angle = shipAngle + error;
 
-        gameObject.transform.eulerAngles = new Vector3(0f, 0f, newAngle);
+        gameObject.transform.eulerAngles = new Vector3(0f, 0f, angle);
         gameObject.transform.position = origin;
         gameObject.name = "Laser Beam";
 
         print("Ship angle " + shipAngle);
         print("Desired angle " + rawAngleToTarget);
         print("Error " + error);
-        print("Pulse angle " + newAngle);
+        print("Pulse angle " + angle);
     }
 
     // Update is called once per frame
 	void Update () {
-        gameObject.transform.Translate(new Vector3(speed * Mathf.Cos(newAngle * Mathf.Deg2Rad) * Time.deltaTime, speed * Mathf.Sin(newAngle * Mathf.Deg2Rad) * Time.deltaTime), Space.World);
+        gameObject.transform.Translate(new Vector3(speed * Mathf.Cos(angle * Mathf.Deg2Rad) * Time.deltaTime, speed * Mathf.Sin(angle * Mathf.Deg2Rad) * Time.deltaTime), Space.World);
         if((gameObject.transform.position - origin).magnitude >= range)
         {
             Destroy(gameObject);
